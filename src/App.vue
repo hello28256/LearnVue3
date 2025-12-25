2573a8df2e204ae81fd1a63bc1a15ef6b364955e
hello28256 <hello28256@gmail.com>
2025-12-25 16:26:39 +0800

    init commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+
+
+<template>
+        <Person/>
+</template>
+
+<script lang="ts">
+    import Person from './components/Person.vue'
+    export default{
+        name:"App",
+        components:{Person}
+    }
+
+</script>
+
+
+
+<style>
+
+</style>
